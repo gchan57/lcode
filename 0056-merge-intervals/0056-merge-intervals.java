3d8a92faff181f4f29b75ede9efb897aeee6fc86
Guna Chandru <134762978+gchan57@users.noreply.github.com>
2026-04-17 18:36:27 +0530

    0056-MERGE-INTERVALS Stats: Time: 10 ms (22.87%), Space: 49 MB (74.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[][] merge(int[][] arr) {
-        int n= arr.length;
         Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
-        ArrayList<int[]> res =new ArrayList<>();
+        ArrayList<int[]> res = new ArrayList<>();
+        res.add(new int[] {arr[0][0],arr[0][1]});
 
-        for(int i=0;i<n;i++){
-            int start = arr[i][0];
-            int end=arr[i][1];
-            if(!res.isEmpty()&&res.get(res.size()-1)[1]>=end){
-                continue;
+        for(int i=0;i<arr.length;i++){
+            int[] last=res.get(res.size()-1);
+            int[] curr=arr[i];
+            if(curr[0]<=last[1]){
+                last[1]=Math.max(last[1],curr[1]);
             }
-        for(int j=i+1;j<n;j++){
-            if(arr[j][0]<=end){
-                end=Math.max(arr[j][1],end);
-            }
-           
-        }
-         res.add(new int[]{start,end});
+            else
+                res.add(new int[]{curr[0],curr[1]});
         }
         int[][] result=new int[res.size()][2];
         for(int i=0;i<res.size();i++){
             result[i]=res.get(i);
         }
-     return result;
+        return result;
     }
 }

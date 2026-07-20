9981b15b6c598ff3053386d974019511953ebb76
Guna Chandru <134762978+gchan57@users.noreply.github.com>
2026-07-20 23:26:55 +0530

    0525-CONTIGUOUS-ARRAY Stats: Time: 24 ms (54.9%), Space: 65.6 MB (26.04%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -5,12 +5,7 @@ class Solution {
         int res=0;
         for(int i=0;i<nums.length;i++){
             
-            if(nums[i]==0){
-                sum+=-1;
-            }
-            else{
-                sum+=1;
-            }
+                        sum+= (nums[i] == 1 ? 1 : -1);
             if(sum==0){
                 res=i+1;
             }
